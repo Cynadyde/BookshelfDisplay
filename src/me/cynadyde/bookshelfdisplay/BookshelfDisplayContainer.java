439908633a824973df6de9312bfa375bbced01ee
Cynadyde <cynadydeindustries@gmail.com>
2019-08-08 01:33:43 -0400

    minor tweaks

--- src/me/cynadyde/bookshelfdisplay/BookshelfDisplayContainer.java
+++ src/me/cynadyde/bookshelfdisplay/BookshelfDisplayContainer.java
@@ -13,14 +13,14 @@ import org.jetbrains.annotations.Nullable;
 public class BookshelfDisplayContainer {
 
     /**
-     * Gets the functional bookshelf at a given block if it exists.
+     * Get the functional bookshelf at a given block if it exists.
      */
     public @Nullable static BookshelfDisplayContainer at(@Nullable Block block) {
 
         Container container = Utils.getAttachedContainer(block);
 
-        // bookshelves container must be a bookshelf block with a chest OR item-frame attached to it...
-        if (block == null || !block.getType().equals(Material.BOOKSHELF) || container == null) {
+        // bookshelfdisplay container must be a bookshelf block with a chest OR item-frame attached to it...
+        if (block == null || block.getType() != Material.BOOKSHELF || container == null) {
             return null;
         }
         return new BookshelfDisplayContainer(block, container);
@@ -29,16 +29,25 @@ public class BookshelfDisplayContainer {
     private Block anchor;
     private Container root;
 
+    /**
+     * Create a representation of a bookshelf's container.
+     */
     public BookshelfDisplayContainer(@NotNull Block block, @NotNull Container container) {
 
         this.anchor = block;
         this.root = container;
     }
 
+    /**
+     * Get the bookshelf's actual block.
+     */
     public @NotNull Block getAnchor() {
         return anchor;
     }
 
+    /**
+     * Get the root of the bookshelf's contents.
+     */
     public @NotNull Container getRoot() {
         return root;
     }
